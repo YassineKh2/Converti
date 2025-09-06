07d4c95eb4791880f5368fed688c3d3f3d3916ff
YassineKh2 <yassinkhemiri02@gmail.com>
2025-09-06 14:40:46 +0100

    Refactored folders to include website

--- electron/Helpers/SanitizeFileName.ts
+++
@@ -1,3 +0,0 @@
-export function SanitizeFileName(name: string): string {
-    return name.replace(/[:\\/*?"<>|]/g, "_");
-}
--- src/type/ConvertStatus.ts
+++
@@ -1,6 +0,0 @@
-export interface ConvertStatus {
-  status: "pending" | "converting" | "completed" | "error";
-  Logs: string[];
-  path?: string;
-  progress: number;
-}
